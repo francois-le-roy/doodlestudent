cd38d30481362ccc18af14e5280bc3512d945653
francois-le-roy <francois.le_roy@outlook.com>
2021-01-30 13:25:33 +0000

    modify scripts

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -6,7 +6,7 @@ cd ../front
 docker build -t frn18/tlc:bunkerized-nginx-front .
 docker push frn18/tlc:bunkerized-nginx-front
 cd ../
-docker system prune -a
+docker system prune -a -f
 docker pull frn18/tlc:quarkus-back-jvm
 docker pull frn18/tlc:bunkerized-nginx-front
 docker-compose up -d
