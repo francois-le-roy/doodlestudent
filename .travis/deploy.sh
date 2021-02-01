d47836a3f0dcd0a8e69a0ef0ad3e5a8f55613328
francois-le-roy <72975383+francois-le-roy@users.noreply.github.com>
2021-02-02 00:55:28 +0100

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,4 @@
 set -e
-- sudo openfortivpn istic-vpn.univ-rennes1.fr:443 -u fleroy -p "${VPN_PASS}" --trusted-cert bf01c9bcba70b3be1f8ae8f941afbc7dcf1d8c81afe7e3d10f48e460c44446c6 --no-routes --no-dns --pppd-no-peerdns &
 git config --global push.default simple
 git remote add production ssh://zprojet@148.60.11.226/home/doodlestudent
 git push production master
